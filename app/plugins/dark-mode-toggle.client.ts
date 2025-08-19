72d991231358f6d8494b4badbb456b47a7623f55
dkvz <github@dkvz.eu>
2025-08-19 23:20:05 +0200

    remove dark-mod-toggle and plugin

--- app/plugins/dark-mode-toggle.client.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtPlugin(async () => {
-  // Apparently we don't need to run customElements.define.
-  //customElements.define('dark-mode-toggle', DarkModeToggle)
-  await import("dark-mode-toggle")
-})
