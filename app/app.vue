3932c982a3c570fbdd79771191ff00b3d6c2b0c6
dkvz <github@dkvz.eu>
2025-08-22 08:59:33 +0200

    move head noscript tag to nuxt.config.ts

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-// It's better to use <ClientOnly> when possible except it's got a big 
-// delay as it only enables after hydration.
-// My "hack" immediately detects when JS is disabled.
-const jsOnlyStyleTag = `<style>._js-only {display: none !important;}<\/style>`
-
-useHead({
-  noscript: [jsOnlyStyleTag]
-})
-</script>
-
 <template>
   <NuxtLayout>
     <NuxtRouteAnnouncer />
