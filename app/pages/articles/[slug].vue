5fb9fdc54deda411ed5ce9be28fdb9fbcdc3d640
dkvz <github@dkvz.eu>
2025-08-22 11:21:42 +0200

    add comment dialog and refactor CSS class for it

--- app/pages/articles/[slug].vue
+++ app/pages/articles/[slug].vue
@@ -7,6 +7,22 @@ useHead({
 </script>
 
 <template>
+
+  <dialog id="comment-dialog">
+    <!--<form method="dialog">
+      <button aria-label="close" class="btn" style="float: right">X</button>
+      </form>-->
+    <h2 class="comment-form-title">Ajouter un commentaire</h2>
+    <form action="#" class="comment-form">
+      <input type="text" class="input" name="comment-author" id="comment-author" placeholder="Votre nom...">
+      <textarea class="input" placeholder="Votre commentaire..." name="comment-comment" id="comment-comment"></textarea>
+      <footer class="comment-form-footer">
+        <button aria-label="close" formmethod="dialog" class="btn">Annuler</button>
+        <button type="submit" class="btn">Envoyer</button>
+      </footer>
+    </form>
+  </dialog>
+
   <article class="content-card content-card--page-card">
     <div class="article-header">
       <h1 class="article-header__title">
