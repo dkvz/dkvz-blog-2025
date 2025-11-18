308b0c8a7ec691a7743980253e2b00671472c104
dkvz <github@dkvz.eu>
2025-11-18 16:14:31 +0100

    Got to change useFetchArticles to allow tags

--- app/pages/tag/[tag]/page/[page].vue
+++ app/pages/tag/[tag]/page/[page].vue
@@ -1,4 +1,26 @@
 <script setup lang="ts">
+import { siteInfo } from '~~/data/site-info'
+
+definePageMeta({
+  validate: validateTagPage,
+})
+
+const route = useRoute()
+// The tag has been validated as non empty string above
+const tag = route.params.tag
+const page = Number(route.params.page) || 1
+const maxItems = siteInfo.maxArticles
+
+const { isOrderAsc, handleToggleOrder } = useOrderToggle()
+
+// TODO: Does the tag print as it should even with URL encoded chars?
+useHead({
+  bodyAttrs: {
+    class: "bg-gradient"
+  },
+  title: `Catégorie ${tag} - Page ${page}`
+})
+
 
 </script>
 
--- app/utils/validators.ts
+++ app/utils/validators.ts
@@ -4,3 +4,15 @@ export const validatePageNumber = (route: RouteLocationNormalizedGeneric): boole
   const page = Number(route.params.page)
   return page > 0 && !isNaN(page);
 }
+
+export const validateTagPage = (route: RouteLocationNormalizedGeneric): boolean => {
+  let tagValid = false
+  if (route.params.tag) {
+    const tag = route.params.toString()
+    if (tag.trim().length > 0) {
+      tagValid = true
+    }
+  }
+
+  return validatePageNumber(route) && tagValid
+}
