cf76ad9463f79ae5f60b7ffc0b71f97d1d779941
dkvz <github@dkvz.eu>
2025-11-22 15:00:52 +0100

    draft of contact page layout

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -21,6 +21,21 @@ useHead({
           <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
         </div>
         <div class="card__body">
+          <h1>Bluesky</h1>
+          <div>
+            <p>J'y traine de temps en temps. Un peu moins depuis que je suis papa.</p>
+          </div>
+        </div>
+        <div class="card__footer">
+          Je sais pas encore que mettre ici, une icone?
+        </div>
+      </article>
+
+      <article class="card card--hoverable card-short">
+        <div class="card__header">
+          <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
+        </div>
+        <div class="card__body">
           <h1>Twitter</h1>
           <div>Je suis plus vraiment dessus</div>
         </div>
@@ -34,7 +49,46 @@ useHead({
           <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
         </div>
         <div class="card__body">
-          <h1>Twitter</h1>
+          <h1>Github</h1>
+          <div>Trop chouette</div>
+        </div>
+        <div class="card__footer">
+          Je sais pas encore que mettre ici, une icone?
+        </div>
+      </article>
+
+      <article class="card card--hoverable card-short">
+        <div class="card__header">
+          <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
+        </div>
+        <div class="card__body">
+          <h1>Email</h1>
+          <div>Je suis plus vraiment dessus</div>
+        </div>
+        <div class="card__footer">
+          Je sais pas encore que mettre ici, une icone?
+        </div>
+      </article>
+
+      <article class="card card--hoverable card-short">
+        <div class="card__header">
+          <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
+        </div>
+        <div class="card__body">
+          <h1>Codepen</h1>
+          <div>Je suis plus vraiment dessus</div>
+        </div>
+        <div class="card__footer">
+          Je sais pas encore que mettre ici, une icone?
+        </div>
+      </article>
+
+      <article class="card card--hoverable card-short">
+        <div class="card__header">
+          <img class="card__large-img" src="/assets/touch/chrome-touch-icon-192x192.png">
+        </div>
+        <div class="card__body">
+          <h1>Youtube</h1>
           <div>Je suis plus vraiment dessus</div>
         </div>
         <div class="card__footer">
